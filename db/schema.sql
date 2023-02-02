7d3797fc6e396a768ad67d2f736622ae9b5ba089
Jag Jhandi <115058975+jagjhandi@users.noreply.github.com>
2023-02-02 18:30:39 -0500

    updated schema.sql to include table

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,9 @@ CREATE DATABASE recipesDB;
 USE recipesDB;
 
 
--- should be more specified tables 
--- recipes tables, categories, ingredients, directions, measurements, 
--- with properties like 
+CREATE TABLE IF NOT EXISTS recipes (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  category VARCHAR(255) NOT NULL,
+  directions TEXT NOT NULL
+);
